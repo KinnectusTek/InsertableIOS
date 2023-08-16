5e8789da8fe2647c9248a7776f164090d9433851
Blake <blake.rogers757@gmail.com>
2023-08-16 08:42:55 -0400

    Initial Commit

---
+++ Insertable/InsertableApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsertableApp.swift
+//  Insertable
+//
+//  Created by Blake Osonduagwueki on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct InsertableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
